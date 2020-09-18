6b696fb588a13bd3b4b6e22d6189d3f4bbec0fd8
Trenton Pottruff <tpottruff@gmail.com>
2020-09-17 20:21:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ bot.on('message', async msg=>{
             
         }
 
-        if (msg.channel.type == 'dm' && msg.author.id == '98917980645109760') {
+        if (msg.channel.type == 'dm' && msg.channel.id == '98917980645109760') {
             console.log(msg.content);
             bot.guilds.resolve('676253496634245169').channels.resolve('676267315381141544').send(msg.content);
             msg.delete();
